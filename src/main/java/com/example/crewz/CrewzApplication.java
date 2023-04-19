551d7dd8e3180b53fcfc99ec71c8e31da9332ea0
HongJongMin <57209066+hjm1593@users.noreply.github.com>
2023-04-19 17:13:37 +0900

    first commit

---
+++ src/main/java/com/example/crewz/CrewzApplication.java
@@ -0,0 +1,13 @@
+package com.example.crewz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrewzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrewzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/crewz/CrewzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crewz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrewzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
