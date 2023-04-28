431ecd7e19b2834b39db9c38a2c850e91c13bc61
HongJongMin <57209066+hjm1593@users.noreply.github.com>
2023-04-28 17:14:25 +0900

    feat: 관리자페이지 작업, 메인 페이지 배너 이미지 추가 - 관리자 페이지 화면작업 - 동아리 신청 리스트 목록 노출 - 승인, 반려 기능 추가 - 반려 시 반려 사유 등록 팝업 - ClubApplyDTO,ClubApplyMapper,ClubAppylyService,ClubApplyServiceImpl 추가

--- src/main/java/environment/project/controller/AdminController.java
+++ src/main/java/environment/project/controller/AdminController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Slf4j
