2e26b317bd9e9dba333bbbc07529a02bd7b7da36
HongJongMin <57209066+hjm1593@users.noreply.github.com>
2023-04-29 12:54:06 +0900

    feat: 전체동아리 페이지, 검색 기능 추가 - 전체동아리 페이지 화면 - 동아리 리스트 목록 조회 - 검색 기능 추가 - all.html 추가 - BoardMapper에 selectServiceToSearch 추가 - MainController에 mapping 추가

--- src/main/java/environment/project/controller/MainController.java
+++ src/main/java/environment/project/controller/MainController.java
@@ -4,16 +4,19 @@ import environment.project.dto.BoardDTO;
 import environment.project.dto.UserDTO;
 import environment.project.service.BoardService;
 import environment.project.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static java.lang.Math.min;
-
+@Slf4j
 @Controller
 public class MainController {
     @Autowired
@@ -21,7 +24,7 @@ public class MainController {
     @Autowired
     private BoardService boardService;
     @GetMapping(path = { "/", "/main" })
-    public String main(Model model) {
+    public String mainPage(Model model) {
         List<UserDTO> list = userService.selectUser();
         List<BoardDTO> boardList = boardService.selectBoard();
         int limit = 4;
@@ -34,6 +37,20 @@ public class MainController {
         model.addAttribute("list", list);
         model.addAttribute("arrBoardList", arrBoardList);
         return "main";
+    }
+
+    @RequestMapping("/all")
+    public String allBoardListToSearch(@RequestParam(required = false) String searchValue, Model model){
+        List<UserDTO> list = userService.selectUser();
+        List<BoardDTO> boardListToSearch = boardService.selectBoardToSearch(searchValue);
+
+        model.addAttribute("list", list);
+        model.addAttribute("boardList", boardListToSearch);
+        model.addAttribute("boardCount",boardListToSearch.size());
+        log.info("boardList: {}",boardListToSearch);
+        log.info("boardCount: {}",boardListToSearch.size());
+
+        return "all";
     }
 }
 
--- src/main/java/environment/project/service/BoardService.java
+++ src/main/java/environment/project/service/BoardService.java
@@ -6,5 +6,8 @@ import environment.project.dto.BoardDTO;
 import java.util.List;
 
 public interface BoardService {
+    int selectBoardCount();
     List<BoardDTO> selectBoard();
+
+    List<BoardDTO> selectBoardToSearch(String boardTitle);
 }
