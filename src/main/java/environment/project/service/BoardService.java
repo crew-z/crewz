df19aac7522593224672e5ffa400f0972479a58f
LEEGWONSEOK <leestart1023@gmail.com>
2023-05-02 12:02:38 +0900

    feat: 동아리 상세 페이지 로직(C,R,U) 설계 완료
    - BoardController : Create, Read, Update, Form(2개)
    - BoardMapper 추가 완료
    - Service, Imple 추가 완료
    - DTO 2개 추가 : BoardCreateDTO, BoardUpdateDTO
    - 예외처리 로직 추가 완료

--- src/main/java/environment/project/service/BoardService.java
+++ src/main/java/environment/project/service/BoardService.java
@@ -1,10 +1,22 @@
 package environment.project.service;
 
 
+import environment.project.dto.BoardCreateDTO;
 import environment.project.dto.BoardDTO;
+import environment.project.dto.BoardUpdateDTO;
 
 import java.util.List;
 
 public interface BoardService {
     List<BoardDTO> selectBoard();
+
+    // GET : 동아리 상세 페이지 read
+    BoardDTO getBoardById(Long boardNo);
+
+    // POST : 동아리 상세 페이지 create
+    int createBoard(BoardCreateDTO boardCreateDTO);
+
+    // PATCH : 동아리 상세 페이지 update
+    boolean updateBoardByBoardNo(BoardUpdateDTO boardUpdateDTO);
+
 }
--- src/main/java/environment/project/service/BoardServiceImpl.java
+++ src/main/java/environment/project/service/BoardServiceImpl.java
@@ -1,6 +1,8 @@
 package environment.project.service;
 
+import environment.project.dto.BoardCreateDTO;
 import environment.project.dto.BoardDTO;
+import environment.project.dto.BoardUpdateDTO;
 import environment.project.mapper.BoardMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,11 +11,27 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class BoardServiceImpl implements BoardService{
+public class BoardServiceImpl implements BoardService {
 
     private final BoardMapper boardMapper;
+
     @Override
     public List<BoardDTO> selectBoard() {
         return boardMapper.selectService();
+    }
+
+    @Override
+    public BoardDTO getBoardById(Long boardNo) {
+        return boardMapper.getBoardById(boardNo);
+    }
+    @Override
+    public int createBoard(BoardCreateDTO boardCreateDTO) {
+        return boardMapper.createBoard(boardCreateDTO);
+    }
+
+    @Override
+    public boolean updateBoardByBoardNo(BoardUpdateDTO boardUpdateDTO) {
+        int rowsAffected = boardMapper.updateBoardByBoardNo(boardUpdateDTO);
+        return rowsAffected > 0;
     }
 }
